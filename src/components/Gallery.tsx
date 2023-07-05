e5019fe8369245cff95541ab9b57324d955322bb
Joseph <joseph161216@gmail.com>
2023-07-05 13:40:15 -0700

    Add gallery section

---
+++ src/components/Gallery.tsx
@@ -0,0 +1,93 @@
+import React from "react";
+
+const Gallery = () => {
+  return (
+    <>
+      <section className="text-gray-400 body-font">
+        <div className="container px-5 py-24 mx-auto">
+          <div className="flex flex-col text-center w-full mb-20">
+            <h1 className="sm:text-3xl text-2xl font-medium title-font mb-4 text-black">
+              Popular Occasions
+            </h1>
+          </div>
+          <div className="flex justify-center flex-wrap -m-4">
+            <div className="p-4 lg:w-1/3 md:w-1/2">
+              <div className="h-full flex flex-col items-center text-center">
+                <img
+                  alt="team"
+                  className="flex-shrink-0 rounded-lg w-full h-56 object-cover object-center mb-4"
+                  src="https://dummyimage.com/202x202"
+                />
+                <div className="w-full">
+                  <h3 className="text-gray-500 mb-3">UI Developer</h3>
+                </div>
+              </div>
+            </div>
+            <div className="p-4 lg:w-1/3 md:w-1/2">
+              <div className="h-full flex flex-col items-center text-center">
+                <img
+                  alt="team"
+                  className="flex-shrink-0 rounded-lg w-full h-56 object-cover object-center mb-4"
+                  src="https://dummyimage.com/202x202"
+                />
+                <div className="w-full">
+                  <h3 className="text-gray-500 mb-3">UI Developer</h3>
+                </div>
+              </div>
+            </div>
+            <div className="p-4 lg:w-1/3 md:w-1/2">
+              <div className="h-full flex flex-col items-center text-center">
+                <img
+                  alt="team"
+                  className="flex-shrink-0 rounded-lg w-full h-56 object-cover object-center mb-4"
+                  src="https://dummyimage.com/202x202"
+                />
+                <div className="w-full">
+                  <h3 className="text-gray-500 mb-3">UI Developer</h3>
+                </div>
+              </div>
+            </div>
+            <div className="p-4 lg:w-1/3 md:w-1/2">
+              <div className="h-full flex flex-col items-center text-center">
+                <img
+                  alt="team"
+                  className="flex-shrink-0 rounded-lg w-full h-56 object-cover object-center mb-4"
+                  src="https://dummyimage.com/202x202"
+                />
+                <div className="w-full">
+                  <h3 className="text-gray-500 mb-3">UI Developer</h3>
+                </div>
+              </div>
+            </div>
+            <div className="p-4 lg:w-1/3 md:w-1/2">
+              <div className="h-full flex flex-col items-center text-center">
+                <img
+                  alt="team"
+                  className="flex-shrink-0 rounded-lg w-full h-56 object-cover object-center mb-4"
+                  src="https://dummyimage.com/202x202"
+                />
+                <div className="w-full">
+                  <h3 className="text-gray-500 mb-3">UI Developer</h3>
+                </div>
+              </div>
+            </div>
+            <div className="p-4 lg:w-1/3 md:w-1/2">
+              <div className="h-full flex flex-col items-center text-center">
+                <img
+                  alt="team"
+                  className="flex-shrink-0 rounded-lg w-full h-56 object-cover object-center mb-4"
+                  src="https://dummyimage.com/202x202"
+                />
+                <div className="w-full">
+                  <h3 className="text-gray-500 mb-3">UI Developer</h3>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Gallery;
