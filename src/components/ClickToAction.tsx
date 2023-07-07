7853bd0c87bfd684b2fb492b0b90af93677319ec
Joseph <joseph161216@gmail.com>
2023-07-07 01:27:28 -0700

    Update click to action

--- src/components/ClickToAction.tsx
+++ src/components/ClickToAction.tsx
@@ -3,28 +3,17 @@ import React from "react";
 const ClickToAction = () => {
   return (
     <>
-      <section className="text-gray-400 bg-gray-900 body-font">
+      <section className="text-black body-font">
         <div className="container px-5 py-24 mx-auto">
           <div className="flex flex-col text-center w-full mb-12">
-            <h1 className="sm:text-3xl text-2xl font-medium title-font mb-4 text-white">
+            <h1 className="m:text-3xl text-2xl font-medium title-font mb-4 text-black">
               Are you a fleet operator?
             </h1>
             <p className="lg:w-2/3 mx-auto leading-relaxed text-base">
               If so, lets get your vehicles listed today at no cost to you!
             </p>
-          </div>
-          <div className="flex lg:w-1/3 w-full sm:flex-row flex-col mx-auto px-8 sm:px-0 items-end sm:space-x-4 sm:space-y-0 space-y-4">
-            <div className="relative sm:mb-0 flex-grow w-full">
-              <input
-                type="email"
-                id="email"
-                name="email"
-                placeholder="Email Address"
-                className="w-full bg-gray-800 bg-opacity-40 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 focus:bg-transparent text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-              />
-            </div>
-            <button className="text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">
-              Button
+            <button className="text-white bg-gray-800 border-0 py-2 px-8 focus:outline-none hover:bg-gray-900 rounded text-lg mt-4 md:max-w-xs md:mx-auto">
+              Find Out More
             </button>
           </div>
         </div>
