7e6ee9b10447cbe5d6931c956da54df40dbee4d3
Joseph <joseph161216@gmail.com>
2023-07-07 01:22:22 -0700

    Add data file

---
+++ src/data.ts
@@ -0,0 +1,91 @@
+import {
+  HiOutlineMagnifyingGlass,
+  HiOutlineCheckCircle,
+  HiOutlineHeart,
+} from "react-icons/hi2";
+
+export const popularOccasions = [
+  {
+    name: "Broadway Show Trips",
+    image: "/images/Broadway Show Trips.jpeg",
+  },
+  { name: "Corporate Events", image: "/images/Corporate Events.jpeg" },
+  { name: "Airport Transfers", image: "/images/Airport Transfers.jpeg" },
+  { name: "Sightseeing Tours", image: "/images/Sightseeing Tours.jpeg" },
+  { name: "Sporting Events", image: "/images/Sporting Events.jpeg" },
+  {
+    name: "Concerts and Festivals",
+    image: "/images/Concerts and Festivals.jpeg",
+  },
+  { name: "Weddings", image: "/images/Weddings.jpeg" },
+  { name: "Night Out/Party Bus", image: "/images/Night Out:Party Bus.jpeg" },
+];
+
+export const iconicDestinations = [
+  { name: "Times Square", image: "/images/Times-Square.jpeg" },
+  {
+    name: "Statue of Liberty and Ellis Island",
+    image: "/images/Statue of Liberty and Ellis Island.jpeg",
+  },
+  { name: "Central Park", image: "/images/Central Park.jpeg" },
+  {
+    name: "Empire State Building",
+    image: "/images/Empire State Building.jpeg",
+  },
+  {
+    name: "Metropolitan Museum of Art (The Met)",
+    image: "/images/Metropolitan Museum of Art (The Met).jpeg",
+  },
+  {
+    name: "Broadway Theatre District",
+    image: "/images/Broadway Theatre District.jpeg",
+  },
+  { name: "Rockefeller Center", image: "/images/Rockefeller Center.jpeg" },
+];
+
+export const rentalVehicles = [
+  {
+    name: "Luxury SUV",
+    description: "Up to 6 people",
+    image: "/images/Luxury SUV.jpeg",
+  },
+  {
+    name: "Limousine",
+    description: "Up to 10 people",
+    image: "/images/Limousine.jpeg",
+  },
+  {
+    name: "Small Party Bus",
+    description: "Up to 16 people",
+    image: "/images/Small Party Bus.jpeg",
+  },
+  {
+    name: "Medium Party Bus",
+    description: "Up to 30 people",
+    image: "/images/Medium Party Bus.jpeg",
+  },
+  {
+    name: "Large Party Bus",
+    description: "Up to 50 people",
+    image: "/images/Large Party Bus.jpeg",
+  },
+];
+
+export const features = [
+  {
+    name: "Search",
+    description:
+      "Search for a party bus, limousine, or any other group vehicle.",
+    icon: HiOutlineMagnifyingGlass,
+  },
+  {
+    name: "Reserve",
+    description: "Message the host directly and book securely online.",
+    icon: HiOutlineCheckCircle,
+  },
+  {
+    name: "Enjoy",
+    description: "Enjoy your chauffeured experience then share your review!",
+    icon: HiOutlineHeart,
+  },
+];
