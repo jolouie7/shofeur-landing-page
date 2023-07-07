8aac6caeae4fee8ed2b31e593b1cfa5b947023b3
Joseph <joseph161216@gmail.com>
2023-07-07 01:25:08 -0700

    Pass props into components

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,18 +5,22 @@ import Feature from "@/components/Feature";
 import RentalVehicle from "@/components/RentalVehicle";
 import ClickToAction from "@/components/ClickToAction";
 import Footer from "@/components/Footer";
+import EmailList from "@/components/EmailList";
+import { popularOccasions, iconicDestinations, rentalVehicles } from "@/data";
 
 export default function Home() {
   return (
     <>
       <Header />
       <Hero />
-      <Gallery />
+      <Gallery dataList={popularOccasions}>Popular Occasions</Gallery>
       <Feature />
-      <Gallery />
-      {/* Replace RentalVehicle with Gallery */}
-      <RentalVehicle />
+      <Gallery dataList={iconicDestinations}>Iconic Destinations</Gallery>
+      <RentalVehicle dataList={rentalVehicles}>
+        Rentals for any group size
+      </RentalVehicle>
       <ClickToAction />
+      <EmailList />
       <Footer />
     </>
   );
