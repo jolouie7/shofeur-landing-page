35877ab84ab7906d3e96124c9084918b07d21391
Joseph <joseph161216@gmail.com>
2023-07-07 01:24:14 -0700

    Add title and description for SEO

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,7 +4,6 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <title>NYC Chauffeured Group Vehicle Rentals | Shofeur</title>
         <meta
           name="description"
           content="Find the best NYC chauffeured group vehicle rentals for your New York group travel needs. Hire group vehicles in NYC for events and party bus rentals."
