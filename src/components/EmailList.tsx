89b4381113dc7892aba51cd16acf79d4949b123e
Joseph <joseph161216@gmail.com>
2023-07-07 20:30:34 -0700

    Update styles

--- src/components/EmailList.tsx
+++ src/components/EmailList.tsx
@@ -20,7 +20,7 @@ const EmailList = () => {
                 id="email"
                 name="email"
                 placeholder="Email Address"
-                className="w-full bg-gray-800 bg-opacity-40 rounded border border-gray-700 focus:border-gray-500 focus:ring-2 focus:ring-gray-900 focus:bg-transparent text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                className="w-full bg-gray-800 bg-opacity-40 rounded border border-gray-700 focus:border-yellow-400 focus:ring-2 focus:ring-gray-900 focus:bg-transparent text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
               />
             </div>
             <button className="text-white bg-gray-500 border-0 py-2 px-8 focus:outline-none hover:bg-gray-600 rounded text-lg">
